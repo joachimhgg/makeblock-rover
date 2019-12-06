010866499aeb369b5a38ea0eb7e612391092cf8a
joachimhgg <j.honegger.achats@gmail.com>
2019-12-06 16:10:18 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Makeblock-rover-docs',
+      version='0.0.2',
+      description='Documentation for the Makeblock Rover',
+      url='https://github.com/joachimhgg/makeblock-rover',
+      author='Joachim Honegger',
+      author_email='joachim.honegger@cpe.fr',
+      license='MIT',
+      zip_safe=False)
